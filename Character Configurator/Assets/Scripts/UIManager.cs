1fb8cb0acd2a6c92a01239250a06e9a217577d37
YelizKB <yelizkurt23@gmail.com>
2022-12-24 23:03:02 +0000

    Position Change depends on screen and some moduifications

--- Character Configurator/Assets/Scripts/UIManager.cs
+++ Character Configurator/Assets/Scripts/UIManager.cs
@@ -7,16 +7,21 @@ public class UIManager : MonoBehaviour
 {
     public GameObject FirstScreen;
     public GameObject SecondScreen;
+    public GameObject ModularChar;
     
     public void OpenFirstScreen()
     {
         FirstScreen.SetActive(true);
         SecondScreen.SetActive(false);
+        ModularChar.transform.position = new Vector3(0.273f, 0, -6.448f);
+        ModularChar.transform.rotation = Quaternion.Euler(0, -170, 0);
     }
     public void OpenSecondScreen() 
     {
         FirstScreen.SetActive(false);
         SecondScreen.SetActive(true);
+        ModularChar.transform.position = new Vector3(-0.62f, 0, -6.291f);
+        ModularChar.transform.rotation = Quaternion.Euler(0, 170, 0);
     }
 
     public void ApplicationClose() { Debug.Log("Application Closed!"); }
