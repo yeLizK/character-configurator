0116578a955e700593215bde305f64ebee0587c8
YelizKB <yelizkurt23@gmail.com>
2022-12-26 22:41:46 +0000

    Camera Focus added

--- Character Configurator/Assets/Scripts/UIManager.cs
+++ Character Configurator/Assets/Scripts/UIManager.cs
@@ -8,13 +8,26 @@ public class UIManager : MonoBehaviour
     public GameObject FirstScreen;
     public GameObject SecondScreen;
     public GameObject ModularChar;
-    
+
+    private Camera mainCamera;
+    private Vector3 cameraDefaultPos;
+    private Vector3 headPos = new Vector3(-0.63f, 1.57f, -6.966f);
+    private Vector3 UpperBodyPos = new Vector3(-0.63f, 1.245f, -7.7f);
+    private Vector3 LowerBodyPos = new Vector3(-0.63f, 0.622f, -8.294f);
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        cameraDefaultPos = mainCamera.transform.position;
+    }
+
     public void OpenFirstScreen()
     {
         FirstScreen.SetActive(true);
         SecondScreen.SetActive(false);
         ModularChar.transform.position = new Vector3(0.273f, 0, -6.448f);
         ModularChar.transform.rotation = Quaternion.Euler(0, -170, 0);
+        SetCameraToDefaultPos();
     }
     public void OpenSecondScreen() 
     {
@@ -23,6 +36,33 @@ public class UIManager : MonoBehaviour
         ModularChar.transform.position = new Vector3(-0.62f, 0, -6.291f);
         ModularChar.transform.rotation = Quaternion.Euler(0, 170, 0);
     }
+
+    public void SetCameraToHeadPos() 
+    {
+        
+        mainCamera.transform.position = Vector3.Slerp(cameraDefaultPos,headPos, 1f );
+
+    }
+    public void SetCameraToUpperBodyPos()
+    {
+
+        mainCamera.transform.position = Vector3.Slerp(cameraDefaultPos, UpperBodyPos, 5f);
+
+    }
+
+    public void SetCameraToLowerBodyPos()
+    {
+
+        mainCamera.transform.position = Vector3.Slerp(cameraDefaultPos, LowerBodyPos, 5f);
+
+    }
+
+    public void SetCameraToDefaultPos()
+    {
+        mainCamera.transform.position = Vector3.Slerp(mainCamera.transform.position, cameraDefaultPos, 5f);
+    }
+
+
 
     public void ApplicationClose() { Debug.Log("Application Closed!"); }
 }
