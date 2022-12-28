7b674522b3bceb9445f013197c879e0415da021e
YelizKB <yelizkurt23@gmail.com>
2022-12-28 22:10:51 +0000

    Character Rotation with IPointers

--- Character Configurator/Assets/Scripts/UIManager.cs
+++ Character Configurator/Assets/Scripts/UIManager.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class UIManager : MonoBehaviour
+public class UIManager : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerMoveHandler
 {
     public GameObject FirstScreen;
     public GameObject SecondScreen;
@@ -17,11 +18,32 @@ public class UIManager : MonoBehaviour
     private Vector3 headPos = new Vector3(-0.63f, 1.57f, -6.966f);
     private Vector3 UpperBodyPos = new Vector3(-0.63f, 1.245f, -7.7f);
     private Vector3 LowerBodyPos = new Vector3(-0.63f, 0.622f, -8.294f);
+    private Vector3 mouseInitialPos;
+    private Vector3 mouseOffset;
+    private Vector3 charRotation;
+    private bool isCharRotating;
+    private float mouseDragSensitivity;
 
     private void Awake()
     {
         mainCamera = Camera.main;
         cameraDefaultPos = mainCamera.transform.position;
+    }
+    private void Start()
+    {
+        charRotation = Vector3.zero;
+        mouseDragSensitivity = 0.4f;
+    }
+    private void Update()
+    {
+        
+        if(isCharRotating)
+        {
+            mouseOffset = (Input.mousePosition - mouseInitialPos);
+            charRotation.y = (mouseOffset.x + mouseOffset.y) * 0.5f;
+            ModularChar.transform.Rotate(charRotation);
+            mouseInitialPos = Input.mousePosition;
+        }
     }
 
     public void OpenFirstScreen()
@@ -74,7 +96,30 @@ public class UIManager : MonoBehaviour
         mainCamera.transform.position = Vector3.Slerp(mainCamera.transform.position, cameraDefaultPos, 5f);
     }
 
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (SecondScreen.activeSelf)
+        {
+            mouseInitialPos = Input.mousePosition;
+            isCharRotating = true;
+        }
+    }
 
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isCharRotating = false;
+    }
+
+    public void OnPointerMove(PointerEventData eventData)
+    {
+        if(isCharRotating)
+        {
+            mouseOffset = (Input.mousePosition - mouseInitialPos);
+            charRotation.y = (mouseOffset.x + mouseOffset.y) * -mouseDragSensitivity;
+            ModularChar.transform.Rotate(charRotation);
+            mouseInitialPos = Input.mousePosition;
+        }
+    }
 
     public void ApplicationClose() { Debug.Log("Application Closed!"); }
 }
